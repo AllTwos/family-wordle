c028d429582c69d30c585082a8c7d666ea39b507
AllTwos <ggmw360@gmail.com>
2024-04-15 12:01:53 -0400

    changes

--- src/misc/misc.js
+++ src/misc/misc.js
@@ -1,5 +1,5 @@
-export const larWord = "grapes";
-export const freeWord = "peach";
+export const larWord = "canoe";
+export const freeWord = "fart";
 export const theme = "";
 
 export const keyboardKeysTop = [
